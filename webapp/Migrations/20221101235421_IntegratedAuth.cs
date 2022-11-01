d905161c2adaf20766bdc9981ab53fab274abbd3
Jonathan Guzmán <jonguz5@hotmail.com>
2022-11-01 17:58:52 -0600

    user credentials migrations

---
+++ webapp/Migrations/20221101235421_IntegratedAuth.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace webapp.Migrations
+{
+    public partial class IntegratedAuth : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "UserCredentials",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "character varying(36)", maxLength: 36, nullable: false),
+                    Email = table.Column<string>(type: "text", nullable: false),
+                    Password = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserCredentials", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserCredentials_Email",
+                table: "UserCredentials",
+                column: "Email",
+                unique: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserProfiles_UserCredentials_Id",
+                table: "UserProfiles",
+                column: "Id",
+                principalTable: "UserCredentials",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserProfiles_UserCredentials_Id",
+                table: "UserProfiles");
+
+            migrationBuilder.DropTable(
+                name: "UserCredentials");
+        }
+    }
+}
